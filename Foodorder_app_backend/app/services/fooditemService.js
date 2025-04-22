db80a2be9c57521bef3f8163bc8ecf8f710aafcc
jclay77 <j.s.clay77@gmail.com>
2025-04-21 23:31:32 -0500

    # Updates to fooditemService.js

--- Foodorder_app_backend/app/services/fooditemService.js
+++ Foodorder_app_backend/app/services/fooditemService.js
@@ -3,22 +3,138 @@ const expressAsyncHandler = require("express-async-handler");
 
 const createFooditem = expressAsyncHandler(async (req, res) => {
   /* COMPLETE TASK 1.a HERE */
+  try {
+    const { name, description, image, CategoryId, CuisineId, isVeg } = req.body;
+    const result = await fooditemRepository.createFooditem(
+      name,
+      description,
+      image,
+      CategoryId,
+      CuisineId,
+      isVeg
+    );
+
+    if (result) {
+      res.status(201).json({
+        message: "Food item created successfully",
+      });
+    } else {
+      res.status(400);
+
+      throw new Error(`Food item creation failed`);
+    }
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Error creating food item",
+
+      error: err.message,
+    });
+  }
 });
 
 const editFooditem = expressAsyncHandler(async (req, res) => {
   /* COMPLETE TASK 1.b HERE */
+  try {
+    const fooditemId = req.params.id;
+    const result = await fooditemRepository.editFooditem(fooditemId, req.body);
+
+    if (result) {
+      res.status(200).json({
+        message: "Food item is successfully edited",
+      });
+    } else {
+      res.status(400);
+
+      throw new Error(`Food item editing failed`);
+    }
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Error editing food item details",
+
+      error: err.message,
+    });
+  }
 });
 
 const deleteFooditem = expressAsyncHandler(async (req, res) => {
   /* COMPLETE TASK 1.c HERE */
+  try {
+    const fooditemId = req.params.id;
+    const result = await fooditemRepository.deleteFooditem(fooditemId);
+
+    if (result) {
+      res.status(200).json({
+        message: "Food item is successfully deleted",
+      });
+    } else {
+      res.status(400);
+
+      throw new Error(`Food item deletion failed`);
+    }
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Error deleting food item",
+
+      error: err.message,
+    });
+  }
 });
 
 const getFooditem = expressAsyncHandler(async (req, res) => {
   /* COMPLETE TASK 1.d HERE */
+  try {
+    const fooditemId = req.params.id;
+    const result = await fooditemRepository.getFooditem(fooditemId);
+
+    if (result) {
+      res.status(200).json({
+        data: result,
+
+        message: "Sucessfully fetched food item details.",
+      });
+    } else {
+      res.status(204);
+
+      throw new Error(
+        `Not able to find the food item based on the food item id: ${fooditemId}`
+      );
+    }
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Error fetching food item details",
+
+      error: err.message,
+    });
+  }
 });
 
 const getAllFooditems = expressAsyncHandler(async (req, res) => {
   /* COMPLETE TASK 1.e HERE */
+  try {
+    const result = await fooditemRepository.getAllFooditems();
+
+    res.status(200).json({
+      data: result,
+
+      message: "Successfully fetched all food items.",
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Error fetching food items",
+
+      error: err.message,
+    });
+  }
 });
 
 module.exports = {
